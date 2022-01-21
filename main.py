7324aebd11684fdb4fb02cc45e08dad54a5ee36a
Fernando Delgadillo <fer.delgadillo.99@gmail.com>
2022-01-20 22:36:38 -0600

    Upload flask to github

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, make_response, redirect, render_template
+
+app = Flask(__name__)
+
+todos = ['Create simple flask project', 'Upload project to azure using terraform', 'Show my web']
+
+@app.route('/')
+def index():
+    user_ip = request.remote_addr
+    response = make_response(redirect('/hello'))
+    response.set_cookie('user_ip', user_ip)
+    return response
+
+@app.route('/hello')
+def hello():
+    user_ip = request.cookies.get('user_ip')
+    context = {
+        'user_ip': user_ip,
+        'todos': todos,
+    }
+
+
+    return render_template('hello.html', **context)
